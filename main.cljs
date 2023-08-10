4ed2fca12821c05e7feae0eacbdf4fee2ab67d6f
Ariya Hidayat <ariya.hidayat@gmail.com>
2023-08-09 23:54:24 -0700

    server & client

---
+++ main.cljs
@@ -0,0 +1,112 @@
+(ns main
+  (:require ["http" :as http]
+            ["url" :as url]
+            ["fs" :as fs]))
+
+(def API_KEY js/process.env.OPENAI_API_KEY)
+
+(def CHAT_API_URL "https://api.openai.com/v1/chat/completions")
+
+(defn http-post [url bearer payload]
+  (js/fetch url (clj->js {:method "POST"
+                          :headers {"Content-Type" "application/json"
+                                    "Authorization" (str "Bearer " bearer)}
+                          :body (js/JSON.stringify (clj->js payload))})))
+
+;; https://platform.openai.com/docs/api-reference/chat
+(defn call-chat-api [messages]
+  (let [payload {:model "gpt-3.5-turbo" :messages messages}
+        promise (http-post CHAT_API_URL API_KEY payload)]
+    (.then promise (fn [response] (.json response)))))
+
+(defonce llm-messages (atom [{:role "system"
+                              :content "You are a helpful assistant."}
+                             {:role "system"
+                              :content "Answer in 50 words or less."}
+                             {:role "user"
+                              :content "Berapa jumlah penduduk Jakarta?"}
+                             {:role "assistant"
+                              :content "Sekitar 10 juta jiwa."}]))
+
+(defn add-message! [role content]
+  (swap! llm-messages conj {:role role :content content}))
+
+(defonce assistant-response (atom {:index 0 :answer "..."}))
+
+(defn print-answer [resolved]
+  (let [response (js->clj resolved :keywordize-keys true)
+        answer (-> response :choices first :message :content)]
+    (add-message! "assistant" answer)
+    (js/console.log "Assistant:" answer)
+    (reset! assistant-response {:index (inc (:index @assistant-response))
+                                :answer answer})))
+
+(defn llm-ask [question]
+  (add-message! "user" question)
+  (js/console.log "User:" question)
+  (let [promise (call-chat-api @llm-messages)]
+    (.then promise print-answer)))
+
+(defn serve-index-html [response]
+  (.writeHead response 200 #js{:content-type "text/html"})
+  (.end response (fs/readFileSync "./public/index.html")))
+
+(defn serve-index-js [response]
+  (.writeHead response 200 #js{:content-type "application/javascript"})
+  (.end response (fs/readFileSync "./public/index.js")))
+
+(defn serve-css [response]
+  (.writeHead response 200 #js{:content-type "text/css"})
+  (.end response (fs/readFileSync "./public/daisyui.css")))
+
+(defn serve-question [request response]
+  (let [parsed-url  (-> request .-url url/parse)
+        question (-> parsed-url .-query js/decodeURIComponent)]
+    (llm-ask question)
+    (.writeHead response 200 #js{:content-type "text/plain"})
+    (.end response (str question))))
+
+(defn serve-answer [response]
+  (.writeHead response 200 #js{:content-type "application/json"})
+  (.end response (js/JSON.stringify (clj->js @assistant-response))))
+
+(defn serve-404 [response]
+  (.writeHead response 404)
+  (.end response))
+
+(defn handler [request response]
+  (let [url (.-url request)]
+    (cond
+      (= url "/") (serve-index-html response)
+      (= url "/index.html") (serve-index-html response)
+      (= url "/index.js") (serve-index-js response)
+      (= url "/daisyui.css") (serve-css response)
+      (= url "/answer") (serve-answer response)
+      (.startsWith url "/question") (serve-question request response)
+      :else (serve-404 response))))
+
+(defn start-server [port]
+  (let [server (http/createServer handler)]
+    (js/console.log "Listening on port" port)
+    (.listen server port)))
+
+(def cli-args
+  (not-empty (js->clj (.slice js/process.argv 3))))
+
+(defn main [_]
+  (if (empty? API_KEY)
+    (js/console.error "No API key, please set OPENAPI_API_KEY!")
+    (start-server 5000)))
+
+(main cli-args)
+
+(comment
+
+  (llm-ask "Sebutkan satu nama masakan khas dari Padang")
+  (llm-ask "Bagaimana cara memasaknya")
+
+  (llm-ask "Apa ibukota Jawa Timur?")
+  (llm-ask "Berapa jaraknya ke Jakarta?")
+
+  ;; moar playboard
+  )
---
+++ public/index.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const $ = (id) => document.getElementById(id);
+
+    // send question to the back-end
+    $('prompt').addEventListener("keydown", function handleKeyInput(event) {
+        if (event.key === "Enter") {
+            const el = $('prompt');
+            const prompt = el.value.trim();
+            el.value = '';
+            if (prompt.length > 0) {
+                appendHumanMessage(prompt);
+                fetch('/question?' + encodeURIComponent(prompt));
+                setTimeout(getAssistantAnswer, 300);
+            }
+        }
+    });
+
+    // get up-to-date answer
+    let assistantIndex = 0;
+    function getAssistantAnswer() {
+        fetch('/answer').then(response => {
+            return response.json();
+        }).then(data => {
+            const { index, answer } = data;
+            if (index > assistantIndex) {
+                assistantIndex = index;
+                appendAssistantMessage(answer);
+            } else {
+                setTimeout(getAssistantAnswer, 100);
+            }
+        })
+    }
+
+    function appendHumanMessage(text) {
+        const msg = document.createElement('div');
+        msg.setAttribute('class', 'chat-bubble chat-bubble-warning');
+        msg.innerText = text;
+        const wrapper = document.createElement('div');
+        wrapper.setAttribute('class', 'chat chat-end');
+        wrapper.appendChild(msg);
+        $('chat').appendChild(wrapper);
+        $("assistant-loading").style.visibility = 'visible';
+    }
+
+    function appendAssistantMessage(text) {
+        const msg = document.createElement('div');
+        msg.setAttribute('class', 'chat-bubble chat-bubble-accent');
+        msg.innerText = text;
+        const wrapper = document.createElement('div');
+        wrapper.setAttribute('class', 'chat chat-start');
+        wrapper.appendChild(msg);
+        $('chat').appendChild(wrapper);
+
+        $("assistant-loading").style.visibility = 'hidden';
+    }
+
+    // poor man's coachmarks
+    setTimeout(() => { appendAssistantMessage('Halo, saya adalah asisten virtual!'); }, 200);
+    setTimeout(() => { appendAssistantMessage('Ada yang bisa saya bantu?'); }, 300);
+    setTimeout(() => { appendHumanMessage('Berapa jumlah penduduk Jakarta?'); }, 500);
+    setTimeout(() => { appendAssistantMessage('Sekitar 10 juta jiwa.'); }, 1100);
+});
